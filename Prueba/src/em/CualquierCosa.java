ea084e1d953a5c7219a445571ccbf56d58e1de25
Micaela López <Micaela Lpez@Victoria-PC>
2021-02-27 01:19:39 -0500

    Modificacion de la nueva clase

---
+++ Prueba/src/em/CualquierCosa.java
@@ -0,0 +1,54 @@
+package em;
+
+	public abstract class CualquierCosa<String> {
+
+		
+		protected String nombre;
+		protected int horasTrabajadas;
+		protected double sueldoPorHora;
+		protected boolean pagado;
+		
+		public CualquierCosa (String nombre, int horasTrabajadas, double sueldoPorHora, boolean pagado) {
+			this.nombre = nombre;
+			this.horasTrabajadas = horasTrabajadas;
+			this.sueldoPorHora = sueldoPorHora;
+			this.pagado = pagado;
+		}
+		
+		// Métodos
+		
+		public int getHorasTrabajadas() {
+			return horasTrabajadas;
+		}
+		
+		public void setHorasTrabajadas(int horasTrabajadas) {
+			this.horasTrabajadas = horasTrabajadas;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public double getSueldoPorHora() {
+			return sueldoPorHora;
+		}
+		
+		public void setSueldoPorHora(double sueldoPorHora) {
+			this.sueldoPorHora = sueldoPorHora;
+		}
+		
+		public boolean getPagado() {
+			return pagado;
+		}
+
+		public void setPagado(boolean pagado) {
+			this.pagado = pagado;
+		}
+		public abstract double calcularSalario(); 
+		
+	}
+
